2a10434b01a6947f2d38488f697b4d7801c4a007
Zhang Cong <zhangcong2009@gmail.com>
2022-07-28 22:52:24 +0800

    add build&run script

--- object_detection.sh
+++ object_detection.sh
@@ -1,5 +1,3 @@
 bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/object_detection:object_detection_tflite
 
-GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection/object_detection_tflite \
-  --calculator_graph_config_file=mediapipe/graphs/object_detection/object_detection_desktop_tflite_graph.pbtxt \
-  --input_side_packets=input_video_path=./test_video.mp4,output_video_path=./output.mp4
+GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/object_detection/object_detection_tflite --calculator_graph_config_file=mediapipe/graphs/object_detection/object_detection_desktop_tflite_graph.pbtxt --input_side_packets=input_video_path=./test_video.mp4,output_video_path=./output.mp4
